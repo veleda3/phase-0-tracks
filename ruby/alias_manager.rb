7cdea144c7ff624e9e9d02979acd6e511e27f4a5
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-19 11:16:31 -0400

    still needs the hash to work

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,49 +8,48 @@
 
 def take_word(word)
   letters = []
-  consonants_array = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z", " "]
+  consonants_array = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z", " "]
   vowels_array = ["a", "e", "i", "o", "u"]
   word = word.downcase.chars
   new_word = word.map! do |letter|
-    if vowels_array.include?(letter)
+    if letter == "b"
+      "z"
+    elsif letter == " "
+      " "
+    elsif letter == "a"
+      "u"
+    elsif vowels_array.include?(letter)
       vowels_array.rotate[vowels_array.index(letter)]
     elsif consonants_array.include?(letter)
       consonants_array.rotate[consonants_array.index(letter)]
-    elsif word[letter] == " "
-      word[letter] == " "
-    elsif letter[index] == "z"
-      letter[index] == "b"
-    elsif letter[index] == "u"
-      letter[index] == "a"
     end
   end
     new_word = new_word.join('').capitalize
     p new_word
    
 end
-      
 
 
-
+  
 
 #build in translator if someone wants to change a nick name to a name.
 
   def nick_name_translator(word)
   letters = []
-  consonants_array = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z", " "]
+  consonants_array = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", " "]
   vowels_array = ["a", "e", "i", "o", "u"]
   word = word.downcase.chars
   new_word = word.map! do |letter|
-    if vowels_array.include?(letter)
+    if letter == "b"
+      "z"
+    elsif letter == " "
+      " "
+    elsif letter == "a"
+      "u"
+    elsif vowels_array.include?(letter)
       vowels_array.rotate(-1)[vowels_array.index(letter)]
-    elsif consonants_array.include?(letter)
+    else consonants_array.include?(letter)
       consonants_array.rotate(-1)[consonants_array.index(letter)]
-    elsif word[letter] == "b"
-      word[letter] == "z"
-    elsif word[letter] == " "
-      word[letter] == " "
-    elsif letter[index] == "a"
-      letter[index] == "u"
     end
   end
     new_word = new_word.join('').capitalize
@@ -58,7 +57,8 @@ end
    
 end
 
-encrypted_word("Fepoimbximi ")
+
+# nick_name_translator()
 
 user_input = " "
 nick_name_array = []
@@ -79,7 +79,8 @@ p name_array
 
 # hard time getting this hash to merge with the two arrays i have
 # name_nick_hash = name_array.zip (nick_name).to_h
-# p name_nick 
+name_nick_hash = Hash[name_array.zip(nick_name)]
+p name_nick_hash
 
 
 
