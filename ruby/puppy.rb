f6ceaaa5f78f6fac7b1c7a780e6408b79ed7cff8
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-21 15:00:26 -0400

    pseudocode of the breed of the puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Xoloitzcuintli zebrali
+
+# CHARACTERISTICS ----------------
+# zebra like stripes
+# hairless
+# Eye count: 8
+# Name: varies
+# Color: brown, with white stripes
+
+# BEHAVIOR -----------------------
+# active
+# intelligent 
+# needs to play a lot
