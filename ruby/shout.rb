822114d2d64449a5a17281cbb52e9734145f78ba
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-23 15:01:56 -0400

    made shouts worked with release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,42 @@
+# module Shout
+
+#   def self.yell_angrily(words)
+#     puts words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     puts words + "!!!" + " :)"
+#   end
+
+# end
+
+# Shout.yell_angrily("I dont want that food")
+# Shout.yell_happily("I love that food")
+
+
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     puts words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     puts words + "!!!" + " :)"
   end
-
 end
 
-Shout.yell_angrily("I dont want that food")
-Shout.yell_happily("I love that food")
+  class HappyPerson
+    include Shout
+  end
+
+  class AngryPerson
+    include Shout
+  end
+
+
+angry_person = AngryPerson.new
+angry_person.yell_angrily("I don't want that food")
+happy_person = HappyPerson.new
+happy_person.yell_happily("I love that food")
+
+
