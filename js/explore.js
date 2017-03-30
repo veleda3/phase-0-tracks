8dea8331572884e6b72685969541020edc48ee2e
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-30 17:53:10 -0400

    reverse done on the word hannah

--- js/explore.js
+++ js/explore.js
@@ -1,10 +1,16 @@
-var str = "Loop over me!";
+function reverse_word(word) {
 
+  var same_word_backwards = "";
+  for (var i = word.length - 1; i >= 0;  i--) {
+    same_word_backwards += word[i];
+  }
+  console.log (same_word_backwards);
+}
 // looking to try to get i to go backward.
 // we are going to iterate through the string 
-// we are going to tell the iteration variable to read backwards
-// after each iteration of the loop -- 
-// all in one tight line of code!
-for (var i = 0; i < str.length; i) {
-  console.log(str[i]);
-}
+// we are going to tell the iteration variable to be the word's 
+// length and start counting from there
+// after each iteration of the variable will go subtract 1 until it gets to 0 
+
+reverse_word('hannah');
+
