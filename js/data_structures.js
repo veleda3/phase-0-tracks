cb76776d2381ad74dc6ae686ce524414efa97350
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-30 18:56:05 -0400

    release 1 done

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,15 @@
+
+// release 0 for 7.2 challenge
 var colors = ["red", "blue", "green", "yellow"];
 var horses = ["James", "John", "Dan", "Carla"];
 colors.push ("purple");
 horses.push("tito");
+
+// release 1 for 7.2 challenge 
 console.log(colors);
 console.log(horses);
+var horse = {};
+ for (var i = 0; i <= colors.length - 1; i++) {
+    horse[horses[i]] = colors[i];
+  }
+  console.log(horse);
