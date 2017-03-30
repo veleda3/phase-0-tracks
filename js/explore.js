738bbfd0c1eb9124981eda63cd7b21427256dbf6
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-30 17:36:20 -0400

    explore.js created

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+var str = "Loop over me!";
+
+// looking to try to get i to go backward.
+// we are going to iterate through the string 
+// we are going to tell the iteration variable to read backwards
+// after each iteration of the loop -- 
+// all in one tight line of code!
+for (var i = 0; i < str.length; i) {
+  console.log(str[i]);
+}
