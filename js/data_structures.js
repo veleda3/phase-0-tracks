232c127d68f00fe4fee05b527bb0603d07b46863
Daniel Velez <velez.a.daniel@gmail.com>
2017-03-30 18:27:35 -0400

    added release 0

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var colors = ["red", "blue", "green", "yellow"];
+var horses = ["James", "John", "Dan", "Carla"];
+colors.push ("purple");
+horses.push("tito");
+console.log(colors);
+console.log(horses);
