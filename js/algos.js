8c3eb6be181d5bf6181a3b194b60a835a0626399
Daniel Velez <velez.a.daniel@gmail.com>
2017-04-01 13:05:18 -0400

    realese 0 on 7.3 done

---
+++ js/algos.js
@@ -0,0 +1,14 @@
+ 
+// Make an array and do a for loop that iterate through each object's length in the array 
+// do so by using the .length command and counting each objects length
+// make a conditional statement that asks if the length of the object is the highest 
+// to print it in the console 
+// release 0
+var longest_word_car = ["tesla","toyota","ford"];
+
+ for (var i = 0; i <= longest_word_car.length - 1; i++) {
+    if (longest_word_car[i].length == 6) 
+    console.log (longest_word_car[i]);
+  }
+
+  
