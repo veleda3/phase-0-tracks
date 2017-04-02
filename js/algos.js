1d6cd9c5a730adab4b2b948f457dc03f70100c2e
Daniel Velez <velez.a.daniel@gmail.com>
2017-04-01 21:38:22 -0400

    release 1 done in 7.3

--- js/algos.js
+++ js/algos.js
@@ -4,11 +4,42 @@
 // make a conditional statement that asks if the length of the object is the highest 
 // to print it in the console 
 // release 0
+function longest_car() {
+
 var longest_word_car = ["tesla","toyota","ford"];
 
  for (var i = 0; i <= longest_word_car.length - 1; i++) {
     if (longest_word_car[i].length == 6) 
     console.log (longest_word_car[i]);
   }
+}
 
-  
+console.log(longest_car());
+
+
+// release 1
+var puppet_3 = {name: 'yellowy', race: "human", talk: false};
+var puppet_2 = {name: 'Reddy', race: "fox", talk: true};
+var puppet_1 = {name: 'Bluey', race: "human", talk: true};
+
+
+function puppet_initialize(puppet1, puppet2) {
+
+  this.puppet1 = puppet1
+  this.puppet2 = puppet2
+  this.comparison = function() {
+
+    if (this.puppet1['name'] || this.puppet1['race'] || this.puppet1['talk'] == this.puppet2['name'] || this.puppet2['race'] || this.puppet2['talk'])
+      console.log(true);
+    else
+      console.log(false);
+  }
+
+}
+
+puppet_initialize(puppet_1, puppet_2);
+comparison();
+
+// release 2
+
+
