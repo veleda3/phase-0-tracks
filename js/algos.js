480e73847303039e453d6213f59177fb63572898
Daniel Velez <velez.a.daniel@gmail.com>
2017-04-02 20:33:57 -0400

    release 2 done with the exception of question 2

--- js/algos.js
+++ js/algos.js
@@ -29,7 +29,11 @@ function puppet_initialize(puppet1, puppet2) {
   this.puppet2 = puppet2
   this.comparison = function() {
 
-    if (this.puppet1['name'] || this.puppet1['race'] || this.puppet1['talk'] == this.puppet2['name'] || this.puppet2['race'] || this.puppet2['talk'])
+    if (this.puppet1['name'] == this.puppet2['name'])
+      console.log(true);
+    else if (this.puppet1['race'] == this.puppet2['race'])
+      console.log(true);
+    else if (this.puppet1['talk'] == this.puppet2['talk'])
       console.log(true);
     else
       console.log(false);
@@ -41,5 +45,35 @@ puppet_initialize(puppet_1, puppet_2);
 comparison();
 
 // release 2
+function word_len(number){
+
+  this.word = number
+    if (this.word > 11){
+    this.word = 0
+    console.log("please try a number between 1 to 10"); 
+  }
+  this.alphabet = "abcdefghijklmnopqrstuvwxyz"
+  this.random_word = []
+    for (var i = 0; i < this.word; i++){
+     this.random_word.push(alphabet.charAt(Math.floor(Math.random() * alphabet.length)));
+      // word.push(this.random_word[i]);
+    }
+  console.log(this.random_word);
+}
+
+// Not able to create an array that passes through as an argument in the function word_len() 
+// and takes each item in the array as the random word generation length. I am having a hard time looping
+//  an array to push random letters and organizing it one to ten through another array
+for (var one_to_ten =[], i = 0; i < 10; ++i) 
+  one_to_ten[i] = i
+  console.log(one_to_ten);
+word_len(6);
+word_len(12);
+// breakdown();
+
+
+
+
+
 
 
