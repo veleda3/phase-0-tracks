0610cc0c1af66371e3ffabd29a0cd86f84e4ae76
Daniel Velez <velez.a.daniel@gmail.com>
2017-04-02 22:06:48 -0400

    jshint used to add all the missing semicolon

--- js/algos.js
+++ js/algos.js
@@ -25,19 +25,19 @@ var puppet_1 = {name: 'Bluey', race: "human", talk: true};
 
 function puppet_initialize(puppet1, puppet2) {
 
-  this.puppet1 = puppet1
-  this.puppet2 = puppet2
+  this.puppet1 = puppet1;
+  this.puppet2 = puppet2;
   this.comparison = function() {
 
-    if (this.puppet1['name'] == this.puppet2['name'])
+    if (this.puppet1.name == this.puppet2.name)
       console.log(true);
-    else if (this.puppet1['race'] == this.puppet2['race'])
+    else if (this.puppet1.race == this.puppet2.race)
       console.log(true);
-    else if (this.puppet1['talk'] == this.puppet2['talk'])
+    else if (this.puppet1.talk == this.puppet2.talk)
       console.log(true);
     else
       console.log(false);
-  }
+  };
 
 }
 
@@ -47,15 +47,15 @@ comparison();
 // release 2
 function word_len(number){
 
-  this.word = number
+  this.word = number;
     if (this.word > 11){
-    this.word = 0
+    this.word = 0;
     console.log("please try a number between 1 to 10"); 
   }
-  this.alphabet = "abcdefghijklmnopqrstuvwxyz"
-  this.random_word = []
+  this.alphabet = "abcdefghijklmnopqrstuvwxyz";
+  this.random_word = [];
     for (var i = 0; i < this.word; i++){
-     this.random_word.push(alphabet.charAt(Math.floor(Math.random() * alphabet.length)));
+     this.random_word.push(this.alphabet.charAt(Math.floor(Math.random() * this.alphabet.length)));
       // word.push(this.random_word[i]);
     }
   console.log(this.random_word);
@@ -65,7 +65,7 @@ function word_len(number){
 // and takes each item in the array as the random word generation length. I am having a hard time looping
 //  an array to push random letters and organizing it one to ten through another array
 for (var one_to_ten =[], i = 0; i < 10; ++i) 
-  one_to_ten[i] = i
+  one_to_ten[i] = i;
   console.log(one_to_ten);
 word_len(6);
 word_len(12);
