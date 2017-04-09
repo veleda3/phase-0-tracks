91160b72df0a515f3b14f67587ba9f8b82fc6c85
Daniel Velez <velez.a.daniel@gmail.com>
2017-04-09 14:21:40 -0400

    8.5 challenge done

--- 8.5_challenge/tracking_trips.rb
+++ 8.5_challenge/tracking_trips.rb
@@ -54,17 +54,41 @@ def potential_new_vacations_and_cost(db, place, cost)
   new_vacations = {place => cost}
   new_vacations.each_pair {|place, cost| 
     places_to_go(db, place, cost)
-    puts "#{place} in #{cost}"
+    puts "your next trip will be #{place} and your budget will be #{cost}"
   }
 end
+
+def update_new_vacation(db, remove_trip)
+   db.execute("DELETE FROM places_to_go WHERE place LIKE (?)", [remove_trip])
+end
+
+
+# driver code
 
 puts "whats a potential place you will like to visit next"
 place = gets.chomp
 puts "How much would you need to budget for this trip?"
 cost = gets.chomp
 potential_new_vacations_and_cost(db, place, cost)
-puts "your information will be save for future usage"
+puts "if you will like to update future trips please type yes or else type no"
+update_trip = gets.chomp
+while update_trip == "yes"
+  puts "here is a list of your future trips"
 
+  future_trips = db.execute(<<-SQL 
+    SELECT place, cost FROM places_to_go;
+    SQL
+    )
+  future_trips.each do |trip|
+    puts "You want to go to #{trip['Place']} and your budget for that trip will be #{trip['cost']} cents"
+  end
+  puts "please type the place you will like to remove from your bucket list"
+  remove_trip = gets.chomp
+  update_new_vacation(db, remove_trip)
+  update_trip = "no"
+end
+
+puts "your information will be save for future usage"
 
 
 
