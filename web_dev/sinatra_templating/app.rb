84cc2dcf9046bb261ee689610381bb24deb08083
Daniel Velez <velez.a.daniel@gmail.com>
2017-04-15 21:36:57 -0400

    9.5 done

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -24,4 +24,14 @@ post '/students' do
   redirect '/'
 end
 
+get '/location' do
+  @students = db.execute("SELECT campus FROM students")
+  erb :location
+end
+
+post '/Email' do
+  db.execute("INSERT INTO Email (Name, Email) VALUES (?,?)", [params['Name'], params['Email'])
+  redirect '/'
+end
+
 # add static resources
